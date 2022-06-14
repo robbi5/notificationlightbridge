e8dfc11305a36d317fae7e075ad309865c560735
Martin Gross <martin@pc-coholic.de>
2022-06-14 14:17:23 +0200

    Add pretixReceiver; Add Readme

--- README.md
+++ README.md
@@ -6,7 +6,14 @@ This project adapts Notification Colors to the led light bar of a WA1053T Androi
 Colors are usually defined [on a NotificationChannel](https://developer.android.com/reference/android/app/NotificationChannel#setLightColor(int)),
 or, on api version < 26 on [the notifications ledARGB property](https://developer.android.com/reference/android/app/Notification#ledARGB).
 
-Note: Flashing the leds is not supported.
+Note: Flashing the LEDs is not supported.
+
+Additionally, using the following generic Broadcast Intents are made available: 
+- `com.robbi5.notificationlightbridge.TURN_OFF`
+- `com.robbi5.notificationlightbridge.SET_COLOR` with an Integer-Extra of `color`
+- `com.robbi5.notificationlightbridge.SET_RAW` with an IntegerList-Extra of `cmd`
+
+The available raw values can be found in the `LED()` companion object.
 
 Installation/Setup
 ------------------
--- app/src/main/java/com/robbi5/notificationlightbridge/SetColorReceiver.kt
+++ app/src/main/java/com/robbi5/notificationlightbridge/SetColorReceiver.kt
@@ -5,19 +5,37 @@ import android.content.Context
 import android.content.Intent
 
 class SetColorReceiver : BroadcastReceiver() {
+    companion object {
+        const val INTENT_TURN_OFF = "com.robbi5.notificationlightbridge.TURN_OFF"
+        const val INTENT_SET_COLOR = "com.robbi5.notificationlightbridge.SET_COLOR"
+        const val INTENT_SET_RAW = "com.robbi5.notificationlightbridge.SET_RAW"
+        val intent_actions = listOf(
+            INTENT_TURN_OFF,
+            INTENT_SET_COLOR,
+            INTENT_SET_RAW
+        )
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
         val led = LED()
         when (intent.action) {
-            "com.robbi5.notificationlightbridge.TURN_OFF" -> {
+            INTENT_TURN_OFF -> {
                 led.send(LED.TURN_OFF)
             }
-            "com.robbi5.notificationlightbridge.SET_COLOR" -> {
+            INTENT_SET_COLOR -> {
                 if (intent.hasExtra("color")) {
                     led.send(
                         listOf(
                             LED.TURN_ON,
                             led.colorMapping(intent.getIntExtra("color", LED.TURN_OFF))
                         )
+                    )
+                }
+            }
+            INTENT_SET_RAW -> {
+                if (intent.hasExtra("cmd")) {
+                    led.send(
+                        intent.getIntArrayExtra("values").toList()
                     )
                 }
             }
